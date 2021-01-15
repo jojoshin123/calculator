9f9dad0e3141f4499b8a8a55af57e802c7f9b0be
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-01-14 21:48:30 -0800

    Operate function

--- App.js
+++ App.js
@@ -5,3 +5,23 @@ function subtract(x, y) { return x - y }
 function multiply(x, y) { return x * y }
 
 function divide(x, y) { return x / y }
+
+function operate(x, y, func) {
+    switch (func) {
+        case "+":
+            return add(x, y);
+            break;
+        case "-":
+            return subtract(x, y);
+            break;
+        case "x":
+        case "*":
+            return multiply(x, y);
+            break;
+        case "/":
+            return divide(x, y);
+            break;
+        default:
+            return "Invalid Function"
+    }
+}
