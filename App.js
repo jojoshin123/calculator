1e8450f88eaaa56d47e96a60615d28ec2513625f
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-01-15 18:47:29 -0800

    Updated styles and overall functionality

--- App.js
+++ App.js
@@ -36,14 +36,58 @@ function populateGrid() {
         grid.appendChild(foo);
         foo.addEventListener('click', () => {
             const display = document.querySelector(".display");
-            if (foo.textContent == "AC") {
-                output.displayString = "";
-            } else if (foo.textContent == "=") {
-                output.displayString = "result";
-            } else {
-                output.displayString += foo.textContent;
+            const val = foo.textContent;
+            if (!isNaN(val) || val == ".") { //if foo is a number or "."
+                if (display.textContent == output.result) {
+                    display.textContent = "";
+                }
+                display.textContent += val;
+            } else if (val == "+" || val == "-" || val == "*" || val == "/" || val == "=") {
+
+                //Set operand1 to display text
+                if (display.textContent.includes(".")) {
+                    output.operand1 = parseFloat(display.textContent);
+                } else {
+                    output.operand1 = parseInt(display.textContent);
+                }
+
+                //calculate current result
+                if (output.result == null) {
+                    output.result = output.operand1;
+                } else {
+                    output.operand1 = operate(output.result, output.operand1, output.operation);
+                    output.result = output.operand1;
+                    display.textContent = output.result;
+                }
+                console.log(`operand1 = ${output.operand1}`);
+                console.log(`operation = ${output.operation}`);
+                console.log(`result = ${output.result}`);
+
+                //store operation
+                if (val != "=") { output.operation = val; }
+
+                if (val == "=") {
+                    display.textContent = output.result;
+                    output.operand1 = output.result;
+                    output.result = null;
+                }
+
+
             }
-            display.textContent = output.displayString;
+            // if (foo.textContent == "=") {
+            //     output.result = operate(output.result, output.operand1, output.operation);
+            //     console.log(`operand1 = ${output.operand1}`);
+            //     console.log(`operation = ${output.operation}`);
+            //     console.log(`result = ${output.result}`);
+            //     display.textContent = output.result;
+            // }
+
+            if (foo.textContent == "AC") {
+                output.operand1 = null;
+                output.operation = "";
+                output.result = null;
+                display.textContent = "";
+            }
         });
     });
 
@@ -51,6 +95,7 @@ function populateGrid() {
 populateGrid();
 
 let output = {
-    displayString: "",
-    result: "",
+    operand1: null,
+    operation: "",
+    result: null,
 }
